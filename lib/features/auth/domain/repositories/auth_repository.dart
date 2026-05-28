4eb4257c46344c230f4f51390d4997d77ea988ee
Ridwan <asepridwansaputra7@gmail.com>
2026-05-28 20:02:29 +0700

    feat: implement user registration flow and UI

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -3,6 +3,8 @@ import '../../../../core/errors/failures.dart';
 import '../entities/user_entity.dart';
 
 abstract class AuthRepository {
-  // Either<Kiri, Kanan>: Kiri untuk Error (Failure), Kanan untuk Sukses (UserEntity)
   Future<Either<Failure, UserEntity>> login(String email, String password);
+  
+  // Tambahkan baris ini untuk kontrak registrasi
+  Future<Either<Failure, UserEntity>> register(UserEntity user);
 }
